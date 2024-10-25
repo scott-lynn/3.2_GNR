97527c0b71a5909d0c8ef73b0afa0221b5d5dbee
Scott Lynn <scottlynn94@gmail.com>
2024-10-25 11:09:20 +0100

    updated logic, added main function

--- code/AGNR.py
+++ code/AGNR.py
@@ -12,6 +12,43 @@ Simulates AGNR using the TB formulation.
 import numpy as np 
 
 ##########################################
+# Functions
+##########################################
+
+def is_odd(num):
+    '''
+    Determines if number is odd.
+
+    Args:
+       num : int, Number for evaluation
+    
+    Returns:
+        True / False : boolean
+    '''
+    mod = num % 2 
+    if mod > 0:
+        return(True)
+    else:
+        return(False)
+
+def is_even(num):
+    '''
+    Determines if number is even.
+
+    Args:
+       num : int, Number for evaluation
+    
+    Returns:
+        True / False : boolean
+    '''
+    mod = num % 2 
+    if mod == 0:
+        return(True)
+    else:
+        return(False)
+
+
+##########################################
 # Main Execution Block
 ##########################################
 def main():
@@ -22,32 +59,75 @@ def main():
     t1: float = 2.7 # eV
 
     # Set k-space range
-    k_start: float= -np.pi/a 
+    k_start: float = -np.pi/a 
     k_end: float = np.pi/a
     k_points: int = 200 
     k = np.linspace(k_start, k_end, k_points)
 
-
     phase = np.exp(1j * k * a)
 
-# Loop 1->N BOTTOM ROW
-for i in range(1, N, 1):
-    # Edge Cases
-    if i == 1:
-        H(i, i+1) = -t 
-        H(i, i+N) = -t
-    if i == N:
-        H(i, i-1) = -t 
-        H(i, i+N) = -t 
+    H = np.zeros(N,N)
 
-    # Odd Cases
-    if is_odd(i): 
-        H(i,i-1) = -t 
-        H(i, i+1) = -t 
-        H(i, i+N) = -t 
+    #LOOP THROUGH K VALUES
 
-    # Even Case
-    if is_even(i):
-        H(i, i-1) = -t 
-        H(i, i+1) = -t 
-        H(i, i+N) = -t
+    # Loop 1->N : BOTTOM ROW
+    for i in range(1, N, 1):
+        # Edge cases
+        # LHS
+        if i == 1:
+            # intra
+            H[i, i+1] = -t1 
+            H[i, i+N] = -t1
+        # RHS
+        if i == N:
+            # intra
+            H[i, i-1] = -t1
+            H[i, i+N] = -t1 
+
+        # Odd cases
+        if is_odd(i): 
+            # intra
+            H[i,i-1] = -t1 
+            H[i, i+1] = -t1 
+            H[i, i+N] = -t1 
+
+        # Even cases
+        if is_even(i):
+            # intra
+            H[i, i-1] = -t1 
+            H[i, i+1] = -t1 
+            # inter
+            H[i, i+N] = -t1 * phase
+
+    # Loop N+1->2N : TOP ROW
+    for i in range(N+1, 2*N, 1):
+        # Edge cases
+        # LHS
+        if i == N+1:
+            # intra
+            H[i, N+1] = -t1 
+            H[i, i-N] = -t1 
+        # RHS
+        if i == 2*N: 
+            # intra
+            H[i, i-1] = -t1 
+            H[i, i-N] = -t1 
+
+        # Odd cases
+        if is_odd(i): 
+            # intra
+            H[i,i-1] = -t1 
+            H[i, i+1] = -t1 
+            H[i, i+N] = -t1 
+
+        # Even cases
+        if is_even(i):
+            # intra
+            H[i, i-1] = -t1 
+            H[i, i+1] = -t1 
+            # inter
+            H[i, i+N] = -t1 * (-phase)
+
+
+
+    
